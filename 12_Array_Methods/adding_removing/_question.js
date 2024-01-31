b5740b5a16a1f892ddbc24e7e39fa6d089c4adad
Niranjan Kumar <niranjan.kumar@talent500.com>
2024-01-31 17:16:59 +0530

    added user defined pop method

--- 12_Array_Methods/adding_removing/_question.js
+++ 12_Array_Methods/adding_removing/_question.js
@@ -1,1 +1,27 @@
 // Mimic the functionality of array remove/add methods
+
+// User defined Pop Method
+let arr = [1,2,3,4,5,6,7,8,9]
+
+function pop(arr){
+    let removedLastItem = arr[arr.length - 1];
+    let newArr = [];
+    for(let i =0; i< arr.length; i++){
+        if(i < arr.length - 1) newArr.push(arr[i])
+    }
+
+    return { removedItem: removedLastItem, newArr }
+}
+
+const { removedItem, newArr }= pop(arr); // arr.pop()
+console.log('removedItem, newArr: ', removedItem, newArr);
+// console.log('original arr: ', arr);
+
+
+// PUSH, SHIFT,  UNSHIFT
+
+
+
+
+
+
