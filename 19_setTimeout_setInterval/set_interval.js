e1c92edbd7a730dfa3533b2ab92592d0ca345d0c
Niranjan Kumar <niranjan.kumar@talent500.com>
2024-02-15 21:11:17 +0530

    added setTimeout and setInterval

--- 19_setTimeout_setInterval/set_interval.js
+++ 19_setTimeout_setInterval/set_interval.js
@@ -7,3 +7,28 @@ The setInterval() method continues calling the function until clearInterval() is
 1 second = 1000 milliseconds.
 
 */
+
+
+
+// function start(){
+//     setInterval(() => {
+//         console.log('Hello');
+//      }, 3000)
+// }
+
+let h1Ele = document.querySelector('h1')
+
+let sec = 0;
+h1Ele.innerHTML = sec
+
+const timerid = setInterval(() => {
+    if(sec == 10){
+        clearInterval(timerid)
+        return;
+    }
+     sec++;
+     h1Ele.innerHTML = sec
+}, 1000);
+
+
+
--- 19_setTimeout_setInterval/set_timeout.js
+++ 19_setTimeout_setInterval/set_timeout.js
@@ -5,29 +5,50 @@ The setTimeout() method calls a function after a number of milliseconds.
 1 second = 1000 milliseconds.
 
 The setTimeout() is executed only once.
-
 If you need repeated executions, use setInterval() instead.
-
 Use the clearTimeout() method to prevent the function from starting.
-
 To clear a timeout, use the id returned from setTimeout()
 
 // ____________ Syntax _____________________
 
 (1) setTimeout(function, milliseconds, param1, param2, ...)
-
 Then you can to stop the execution by calling clearTimeout():
-
 (2) clearTimeout(myTimeout);
-
-
 
 
 */
 
+let timerId1 = ''
+let timerId2 = ''
 
-
-function sayMessage () {
-    alert("hello")
+function sayMessage (timer) {
+    alert(`hello from ${timer}`)
 }
+function firstTimeout(time){
+   timerId1 = setTimeout(sayMessage, time, 'timer1')
+   console.log('timerId1: ', timerId1);
+}
+function secondTimeout(time){
+   timerId2 = setTimeout(sayMessage, time, 'timer2')
+}
+
+function stopFirstTimeout(){
+    console.log('timerid 1', timerId1);
+    clearTimeout(timerId1)
+}
+
+function stopSecondTimeout(){
+    console.log('timerid 2', timerId2);
+
+    clearTimeout(timerId2)
+}
+// 1000ms = 1 sec
+
+// console.log("Before setTimeout")
+
+// setTimeout(() => {
+//    console.log("hello")
+// }, 5000)
+
+// console.log("After setTimeout")
 
