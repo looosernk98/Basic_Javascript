d2b8ae993406d2e66e837dd78fd3ba2dfa93829d
Niranjan Kumar <niranjan.kumar@talent500.com>
2024-03-31 14:37:09 +0530

    added pagination design

--- _projects/table/pagination.js
+++ _projects/table/pagination.js
@@ -3,8 +3,14 @@ let totalBox = ''
 for(let i=0;i<=9;i++){
    let first =  
     `
-   <div class = "ten-box">${i+1}</div>
+    <div class = "ten-box">${i+1}</div>
    `
+
+
+
+   // let ele = document.createElement('div')
+   // ele.classList.add('ten-box')
+   // ele.innerHTML = i+1
    
    totalBox += first
   }
